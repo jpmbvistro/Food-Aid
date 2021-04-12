742a9821a3a007e7690bb57711b5c222df398dd3
Juan <jpmbvistro@gmail.com>
2021-04-12 07:41:49 -0400

    adding success post image

--- public/js/footer.js
+++ public/js/footer.js
@@ -5,8 +5,8 @@ document.querySelector('#addAidButton').addEventListener('click', element => {
 
 document.querySelector('#submit-new-aid').addEventListener('click', element => {
   let form = element.currentTarget.parentElement
-  console.log('STUFF')
-  console.log(form.querySelector("input[name='title']").value)
+  // console.log('STUFF')
+  // console.log(form.querySelector("input[name='title']").value)
   fetch('aid', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
@@ -18,11 +18,61 @@ document.querySelector('#submit-new-aid').addEventListener('click', element => {
     expiration: form.querySelector("input[name='expiration']").value
   })
   .then(response => {
-    if (response.ok) return response.json()
+    if (response.ok) {
+      document.querySelector('footer').classList.remove('expand')
+      return response.json()
+
+    }
   })
   .then(data => {
+    submitAidAnimation('success')
     console.log(data)
 
 
   })
 })
+
+function submitAidAnimation(res){
+  let flashMessageBackground = document.createElement('div')
+  flashMessageBackground.classList.add('flash-message-container')
+  flashMessageBackground.id = 'flash-message-container'
+
+  let flashMessageCard = document.createElement('div')
+  flashMessageCard.classList.add('flash-message-card','flex-container','flex-column','flex-xy-center')
+
+
+  let flashMessageImage = document.createElement('img')
+  let flashMessageHeader = document.createElement('h1')
+  let flashMessageBody = document.createElement('p')
+
+  let flashButton = document.createElement('button')
+  flashButton.classList.add('primary-button')
+  flashButton.addEventListener('click',removeFlashMessage)
+  flashButton.innerText = 'Close'
+
+  if(res==='success'){
+    flashMessageHeader.innerText = 'Nice!'
+    flashMessageBody.innerText = 'Thanks for contributing to the Project'
+    flashMessageImage.src = 'img/icons/success-icon.png'
+    flashMessageImage.setAttribute('alt', 'Success Image')
+  } else {
+    flashMessageHeader.innerText = 'Oops!'
+    flashMessageBody.innerText = "Seems like we weren't able to do that. We'll be working on that!"
+    flashMessageImage.src = 'img/icons/oops-icon.png'
+    flashMessageImage.setAttribute('alt', 'Failure Image')
+  }
+
+  flashMessageCard.appendChild(flashMessageImage)
+  flashMessageCard.appendChild(flashMessageHeader)
+  flashMessageCard.appendChild(flashMessageBody)
+  flashMessageCard.appendChild(flashButton)
+  flashMessageBackground.appendChild(flashMessageCard)
+  document.querySelector('body').appendChild(flashMessageBackground)
+  setTimeout(()=>flashMessageBackground.classList.add('animate'),500)
+}
+
+function removeFlashMessage() {
+  let target = document.querySelector('#flash-message-container')
+  target.classList.add('remove-animation')
+  setTimeout(()=>target.remove(),500)
+}
