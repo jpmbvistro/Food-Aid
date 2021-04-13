7f60437c36549c6000dd0b18beda88b5ee48f5a1
Juan <jpmbvistro@gmail.com>
2021-04-13 06:59:57 -0400

    Moved success flash message animation triggering to partial js file

--- public/js/footer.js
+++ public/js/footer.js
@@ -31,3 +31,48 @@ document.querySelector('#submit-new-aid').addEventListener('click', element => {
 
   })
 })
+
+function submitAidAnimation(res){
+  let flashMessageBackground = document.createElement('div')
+  flashMessageBackground.classList.add('flash-message-container')
+  flashMessageBackground.id = 'flash-message-container'
+
+  let flashMessageCard = document.createElement('div')
+  flashMessageCard.classList.add('flash-message-card','flex-container','flex-column','flex-xy-center')
+
+
+  let flashMessageImage = document.createElement('img')
+  let flashMessageHeader = document.createElement('h1')
+  let flashMessageBody = document.createElement('p')
+
+  let flashButton = document.createElement('button')
+  flashButton.classList.add('primary-button')
+  flashButton.addEventListener('click',removeFlashMessage)
+  flashButton.innerText = 'Close'
+
+  if(res==='success'){
+    flashMessageHeader.innerText = 'Nice!'
+    flashMessageBody.innerText = 'Thanks for contributing to the Project'
+    flashMessageImage.src = 'img/icons/success-icon.png'
+    flashMessageImage.setAttribute('alt', 'Success Image')
+  } else {
+    flashMessageHeader.innerText = 'Oops!'
+    flashMessageBody.innerText = "Seems like we weren't able to do that. We'll be working on that!"
+    flashMessageImage.src = 'img/icons/oops-icon.png'
+    flashMessageImage.setAttribute('alt', 'Failure Image')
+  }
+
+  flashMessageCard.appendChild(flashMessageImage)
+  flashMessageCard.appendChild(flashMessageHeader)
+  flashMessageCard.appendChild(flashMessageBody)
+  flashMessageCard.appendChild(flashButton)
+  flashMessageBackground.appendChild(flashMessageCard)
+  document.querySelector('body').appendChild(flashMessageBackground)
+  setTimeout(()=>flashMessageBackground.classList.add('animate'),500)
+}
+
+function removeFlashMessage() {
+  let target = document.querySelector('#flash-message-container')
+  target.classList.add('remove-animation')
+  setTimeout(()=>target.remove(),500)
+}
